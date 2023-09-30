db6ff78ab71ac5f7de2f4ca09bee70a9d42262e1
Ryhverse <128314324+Ryhverse@users.noreply.github.com>
2023-09-30 19:06:57 +0200

    added script

---
+++ edge_tts/script.py
@@ -0,0 +1,426 @@
+# edge_tts for text-generation-webui
+
+import time
+from pathlib import Path
+import random
+import sys
+import os
+import traceback
+import librosa
+import html
+
+import subprocess
+
+import gradio as gr
+import torch
+import edge_tts
+import asyncio
+from scipy.io import wavfile
+import numpy as np
+
+from modules import chat, shared, ui_chat
+from modules.utils import gradio
+
+from fairseq import checkpoint_utils
+
+sys.path.append("extensions/edge_tts")
+
+import tts_preprocessor
+from rmvpe import RMVPE
+from vc_infer_pipeline import VC
+from lib.infer_pack.models import (
+    SynthesizerTrnMs256NSFsid,
+    SynthesizerTrnMs256NSFsid_nono,
+    SynthesizerTrnMs768NSFsid,
+    SynthesizerTrnMs768NSFsid_nono,
+)
+from config import Config
+
+torch._C._jit_set_profiling_mode(False)
+
+params = {
+    'activate': True,
+    'speaker': 'en-US-MichelleNeural',
+    'language': 'en',
+    'show_text': True,
+    'autoplay': True,
+    'rvc': True,
+    'rvc_model': 'RikkuV2/RikkuV2.pth',
+    'transpose': 0,
+    'index_rate': 1,
+    'protect': 0.21,
+    'speed':0,
+}
+current_params = params.copy()
+
+voices = []
+rvc_models = []
+rvc_config = Config()
+hubert_model = None
+rmvpe_model = None
+
+
+def load_hubert():
+    global hubert_model
+    models, _, _ = checkpoint_utils.load_model_ensemble_and_task(
+        ["extensions/edge_tts/models/hubert_base.pt"],
+        suffix="",
+    )
+    hubert_model = models[0]
+    hubert_model = hubert_model.to(rvc_config.device)
+    if rvc_config.is_half:
+        hubert_model = hubert_model.half()
+    else:
+        hubert_model = hubert_model.float()
+    return hubert_model.eval()
+
+
+def get_all_paths(relative_directory):
+    folders = []
+    files = []
+
+    for dirpath, dirnames, filenames in os.walk(relative_directory):
+        for dirname in dirnames:
+            folders.append(os.path.relpath(os.path.join(dirpath, dirname), relative_directory))
+        for filename in filenames:
+            files.append(os.path.relpath(os.path.join(dirpath, filename), relative_directory))
+
+    return folders, files
+
+
+def remove_tts_from_history(history):
+    for i, entry in enumerate(history['internal']):
+        history['visible'][i] = [history['visible'][i][0], entry[1]]
+
+    return history
+
+
+def toggle_text_in_history(history):
+    for i, entry in enumerate(history['visible']):
+        visible_reply = entry[1]
+        if visible_reply.startswith('<audio'):
+            if params['show_text']:
+                reply = history['internal'][i][1]
+                history['visible'][i] = [history['visible'][i][0], f"{visible_reply.split('</audio>')[0]}</audio>\n\n{reply}"]
+            else:
+                history['visible'][i] = [history['visible'][i][0], f"{visible_reply.split('</audio>')[0]}</audio>"]
+
+    return history
+
+
+def state_modifier(state):
+    if not params['activate']:
+        return state
+
+    state['stream'] = False
+    return state
+
+
+def input_modifier(string, state):
+    if not params['activate']:
+        return string
+
+    shared.processing_message = "*Is recording a voice message...*"
+    return string
+
+
+def history_modifier(history):
+    # Remove autoplay from the last reply
+    if len(history['internal']) > 0:
+        history['visible'][-1] = [
+            history['visible'][-1][0],
+            history['visible'][-1][1].replace('controls autoplay>', 'controls>')
+        ]
+
+    return history
+
+
+def output_modifier(string, state):
+    if not params['activate']:
+        return string
+
+    original_string = string
+
+    original_string = string
+    string = tts_preprocessor.replace_invalid_chars(html.unescape(string))
+    string = tts_preprocessor.replace_abbreviations(string)
+    string = tts_preprocessor.clean_whitespace(string)
+
+    if string == '':
+        string = '*Empty reply, try regenerating*'
+    else:
+        #output_file = Path(f'extensions/edge_tts/outputs/{int(time.time())}.mp3')
+        # Assuming mp3_file is the path to your original MP3 file
+        output_file = Path(f'extensions/edge_tts/outputs/{int(time.time())}.mp3')
+        wav_file = Path(f'extensions/edge_tts/outputs/myOOGA_TTSaudio.wav')
+        
+        print(f'Outputting audio to {str(output_file)}')
+
+        if params['speed'] < 0 :
+            speed_str = f"{params['speed']}%"
+        elif params['speed'] == 0 :
+            speed_str = f"+{params['speed']}%"
+        else :
+            speed_str = f"+{params['speed']}%"
+        print("Speed String:", speed_str)
+
+        communicate = edge_tts.Communicate(string,params['speaker'], rate=speed_str)
+        #communicate = edge_tts.Communicate(string, params['speaker'])
+        asyncio.run(communicate.save(output_file))
+
+        if (params['rvc'] is True):
+            print('Running RVC')
+            audio = tts(output_file, params['rvc_model'], params['transpose'], 'rmvpe', params['index_rate'], params['protect'],params['speed'])
+            wavfile.write(output_file, 44100, audio.astype(np.int16))
+
+            subprocess.run(['ffmpeg', '-y','-i', output_file, wav_file])
+
+           
+        autoplay = 'autoplay' if params['autoplay'] else ''
+        string = f'<audio src="file/{output_file.as_posix()}" controls {autoplay}></audio>'
+        #if params['show_text']:
+            #string += f'\n\n{original_string}'
+
+    shared.processing_message = "*Is typing...*"
+    #return string
+    return original_string
+
+
+def get_voices():
+    voices = asyncio.run(edge_tts.list_voices())
+    names = [x['ShortName'] for x in voices]
+    return names
+
+
+def random_sentence():
+    with open(Path("extensions/edge_tts/harvard_sentences.txt")) as f:
+        return random.choice(list(f))
+
+
+def voice_preview(preview_text):    
+    global params
+
+    string = preview_text or random_sentence()
+
+    output_file = Path('extensions/edge_tts/outputs/voice_preview.mp3')
+
+
+    #print("we are checking speed :", params['speed'][0])
+   
+    if params['speed'] < 0 :
+        speed_str = f"{params['speed']}%"
+    elif params['speed'] == 0 :
+        speed_str = f"+{params['speed']}%"
+    else :
+        speed_str = f"+{params['speed']}%"
+    print("Speed String:", speed_str)
+    communicate = edge_tts.Communicate(string,params['speaker'], rate=speed_str)
+    
+    #communicate = edge_tts.Communicate(string, params['speaker'])
+    asyncio.run(communicate.save(output_file))
+
+    if (params['rvc'] is True):
+        audio = tts(output_file, params['rvc_model'], params['transpose'], 'rmvpe', params['index_rate'], params['protect'],params['speed'])
+        wavfile.write(output_file, 44100, audio.astype(np.int16))
+    return print("wav file saved")
+    #return f'<audio src="file/{output_file.as_posix()}?{int(time.time())}" controls autoplay></audio>'
+
+
+def setup():
+    global voices, current_params, rvc_models, rmvpe_model, hubert_model
+    
+    for i in params:
+        if params[i] != current_params[i]:
+            current_params = params.copy()
+            break
+    voices = get_voices()
+    folders, files = get_all_paths('extensions/edge_tts/rvc_models')
+    rvc_models = files
+
+    print("Loading hubert model...")
+    hubert_model = load_hubert()
+    print("Hubert model loaded.")
+
+    print("Loading rmvpe model...")
+    rmvpe_model = RMVPE("extensions/edge_tts/models/rmvpe.pt", rvc_config.is_half, rvc_config.device)
+    print("rmvpe model loaded.")
+
+def ui():
+    # Gradio elements
+    with gr.Accordion("Edge TTS"):
+        with gr.Row():
+            activate = gr.Checkbox(value=params['activate'], label='Activate TTS')
+            autoplay = gr.Checkbox(value=params['autoplay'], label='Play TTS automatically')
+
+        show_text = gr.Checkbox(value=params['show_text'], label='Show message text under audio player')
+        voice = gr.Dropdown(value=params['speaker'], choices=voices, label='TTS voice')
+
+        with gr.Row():
+            rvc = gr.Checkbox(value=params['rvc'], label='Use RVC')
+            model_dropdown = gr.Dropdown(value=params['rvc_model'], choices=rvc_models, label='RVC Model')
+
+        with gr.Row():
+            preview_text = gr.Text(show_label=False, placeholder="Preview text", elem_id="coqui_preview_text")
+            preview_play = gr.Button("Preview")
+            preview_audio = gr.HTML(visible=False)
+
+        with gr.Column():
+            transpose = gr.Slider(minimum=-50, maximum=50, value=params['transpose'], step=1, label='Transpose')
+            index_rate = gr.Slider(minimum=0, maximum=1, value=params['index_rate'], step=0.01, label='Index Rate')
+            protect = gr.Slider(minimum=0, maximum=0.5, value=params['protect'], step=0.01, label='Protect')
+            speed = gr.Slider(minimum=-100,maximum=100,value=0,step=5,label="Speech speed (%)")
+
+    if shared.is_chat():
+        # Toggle message text in history
+        show_text.change(
+            lambda x: params.update({"show_text": x}), show_text, None).then(
+            toggle_text_in_history, gradio('history'), gradio('history')).then(
+            chat.save_history, gradio('history', 'unique_id', 'character_menu', 'mode'), None).then(
+            chat.redraw_html, gradio(ui_chat.reload_arr), gradio('display'))
+            
+    # Event functions to update the parameters in the backend
+    activate.change(lambda x: params.update({"activate": x}), activate, None)
+    autoplay.change(lambda x: params.update({"autoplay": x}), autoplay, None)
+    voice.change(lambda x: params.update({"speaker": x}), voice, None)
+    rvc.change(lambda x: params.update({"rvc": x}), rvc, None)
+    model_dropdown.change(lambda x: params.update({"rvc_model": x}), model_dropdown, None)
+    transpose.change(lambda x: params.update({"transpose": x}), transpose, None)
+    index_rate.change(lambda x: params.update({"index_rate": x}), index_rate, None)
+    protect.change(lambda x: params.update({"protect": x}), protect, None)
+    speed.change(lambda x: params.update({"speed": x}), speed, None)
+
+    # Play preview
+    preview_text.submit(voice_preview, preview_text, preview_audio)
+    preview_play.click(voice_preview, preview_text, preview_audio)
+
+# RVC functions, retrieved via https://github.com/litagin02/rvc-tts-webui
+
+
+def model_data(model_name):
+    # global n_spk, tgt_sr, net_g, vc, cpt, version, index_file
+    pth_path = f'extensions/edge_tts/rvc_models/{model_name}'
+    
+    cpt = torch.load(pth_path, map_location="cpu")
+    tgt_sr = cpt["config"][-1]
+    cpt["config"][-3] = cpt["weight"]["emb_g.weight"].shape[0]  # n_spk
+    if_f0 = cpt.get("f0", 1)
+    version = cpt.get("version", "v1")
+    if version == "v1":
+        if if_f0 == 1:
+            net_g = SynthesizerTrnMs256NSFsid(*cpt["config"], is_half=True)
+        else:
+            net_g = SynthesizerTrnMs256NSFsid_nono(*cpt["config"])
+    elif version == "v2":
+        if if_f0 == 1:
+            net_g = SynthesizerTrnMs768NSFsid(*cpt["config"], is_half=True)
+        else:
+            net_g = SynthesizerTrnMs768NSFsid_nono(*cpt["config"])
+    else:
+        raise ValueError("Unknown version")
+    del net_g.enc_q
+    net_g.load_state_dict(cpt["weight"], strict=False)
+    print("Model loaded")
+    net_g.eval().to('cuda')
+    net_g = net_g.half()
+    vc = VC(tgt_sr, rvc_config)
+    # n_spk = cpt["config"][-3]
+
+    #index_name = ''
+    #index_file = f'extensions/edge_tts/rvc_models/{index_name}'
+
+    # Check if the index_file ends with the model_name and '.index'
+    #if index_file.endswith(f'{model_name}.index'):
+        #print("Index file matches the expected pattern.")
+    #else:
+        #print("Index file does not match the expected pattern.")
+
+
+    model_root = "extensions/edge_tts/rvc_models"
+
+    # Get a list of directories in the model_root
+    model_directories = [d for d in os.listdir(model_root) if os.path.isdir(os.path.join(model_root, d))]
+
+    for model_name in model_directories:
+        model_path = os.path.join(model_root, model_name)
+
+        # Check if both .pth and .index files exist in the directory
+        pth_file = os.path.join(model_path, f'{model_name}.pth')
+        index_file = os.path.join(model_path, f'{model_name}.index')
+
+        if os.path.exists(pth_file) and os.path.exists(index_file):
+            print(f"Model and index files found for model {model_name}")
+            # Your additional code here using pth_file and index_file
+        else:
+            print(f"Skipping {model_name} as it does not have both .pth and .index files.")
+
+    return tgt_sr, net_g, vc, version, index_file, if_f0
+
+
+def tts(
+    output_file,
+    model_name,
+    f0_up_key=1,
+    f0_method='rmvpe',
+    index_rate=1,
+    protect=0.23,
+    filter_radius=3,
+    resample_sr=0,
+    rms_mix_rate=0.25,
+):
+    try:
+        global hubert_model, rmvpe_model
+
+        edge_output_filename = output_file
+
+        tgt_sr, net_g, vc, version, index_file, if_f0 = model_data(model_name)
+        
+        audio, sr = librosa.load(edge_output_filename, sr=16000, mono=True)
+        duration = len(audio) / sr
+
+        print(f"Audio duration: {duration}s")
+    
+        f0_up_key = int(f0_up_key)
+
+        if not hubert_model:
+            load_hubert()
+        if f0_method == "rmvpe":
+            vc.model_rmvpe = rmvpe_model
+        times = [0, 0, 0]
+        audio_opt = vc.pipeline(
+            hubert_model,
+            net_g,
+            0,
+            audio,
+            edge_output_filename,
+            times,
+            f0_up_key,
+            f0_method,
+            index_file,
+            # file_big_npy,
+            index_rate,
+            if_f0,
+            filter_radius,
+            tgt_sr,
+            resample_sr,
+            rms_mix_rate,
+            version,
+            protect,
+            None,
+        )
+        if tgt_sr != resample_sr >= 16000:
+            tgt_sr = resample_sr
+        info = f"Success."
+        print(info)
+        return audio_opt
+    except EOFError:
+        info = (
+            "It seems that the edge-tts output is not valid. "
+            "This may occur when the input text and the speaker do not match. "
+            "For example, maybe you entered Japanese (without alphabets) text but chose non-Japanese speaker?"
+        )
+        print(info)
+    
+    except:
+        info = traceback.format_exc()
+        print(info)
+        return info, None, None
